d6a210a4d669f5e53590aab624746b352643a303
Abhishek <abhishek@deeplogicai.tech>
2025-05-05 22:56:22 +0530

    Remove commented-out subdomain checks from baseDomain.js

--- frontend/src/common/baseDomain.js
+++ frontend/src/common/baseDomain.js
@@ -1,10 +1,11 @@
 export let subDomain = window.location.host.split(".")[0];
-const allowedSubDomains = [
-	"localhost",
-	"localhost:5173",
-	"www",
-];
-export const subDomainCheck = () => allowedSubDomains.includes(subDomain);
+// const allowedSubDomains = [
+// 	"localhost",
+// 	"localhost:5173",
+// 	"www",
+
+// ];
+// export const subDomainCheck = () => allowedSubDomains.includes(subDomain);
 
 const baseDomain = {
 	route:"http://localhost:5000",
